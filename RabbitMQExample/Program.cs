d08c5acb2590dccc93ada896a0f0ee35466b5dc7
GustavoRosauro <gustavosrosauro@gmail.com>
2021-02-28 11:00:32 -0300

    exemplo Rabbit

---
+++ RabbitMQExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var chanel = connection.CreateModel())
+                {
+                    chanel.QueueDeclare(queue: "Mensagem",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                    string mensagem = "Mensagem foi enviada para o Rabbit";
+                    var body = Encoding.UTF8.GetBytes(mensagem);
+                    chanel.BasicPublish(exchange: "",
+                                        routingKey:"Mensagem",
+                                        basicProperties: null,
+                                        body: body);
+
+                    Console.WriteLine(" [x] Enviado {0}", mensagem);
+                    Console.WriteLine(" [x] Aperte enter para sair ");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQRecive/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQRecive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var chanel = connection.CreateModel())
+                {
+                    chanel.QueueDeclare(queue:"Mensagem",
+                                        durable:false,
+                                        exclusive:false,
+                                        autoDelete:false,
+                                        arguments:null);
+                    var consumer = new EventingBasicConsumer(chanel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var mensagem = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Recebeu  {0}", mensagem);
+                    };
+                    chanel.BasicConsume(queue:"Mensagem",
+                                        autoAck:true,
+                                        consumer:consumer);
+
+                    Console.WriteLine(" Aperte [enter] para sair ");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
